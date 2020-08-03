b7295dfc3f01ae2ab9b96769f9802a346c7f2f18
MayronSouza <mayron.souza@gmz.solutions>
2020-08-03 19:41:39 -0300

    Customizacao da Landing Page seguindo o padrao Mobile First

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -7,13 +7,15 @@ import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
+import './styles.css'
+
 function Landing() {
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
         <div className="logo-container">
           <img src={logoImg} alt="Proffy"/>
-          <h2></h2>
+          <h2>Sua plataforma de estudos on-line</h2>
         </div>
         <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
         <div className="buttons-container">
