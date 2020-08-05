fa1fc601aade50b40536854055de9ee0cefac22a
MayronSouza <mayron.souza@gmz.solutions>
2020-08-05 16:41:51 -0300

    Finalizando a customizacao das pages e linkando a Landing Page ao back-end usando axios

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
+// Assets
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
-
-import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import studyIcon from '../../assets/images/icons/study.svg'
 
+// Services
+import api from '../../services/api'
+
+// Styles
 import './styles.css'
 
 function Landing() {
+  const [totalConnections, setTotalConnections] =useState(0)
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+        const { total } = response.data
+
+        setTotalConnections(total)
+      })        
+  }, [])
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -30,7 +44,7 @@ function Landing() {
           </Link>
         </div>
         <span className="total-connections">
-          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+          Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
         </span>
       </div>
     </div>
